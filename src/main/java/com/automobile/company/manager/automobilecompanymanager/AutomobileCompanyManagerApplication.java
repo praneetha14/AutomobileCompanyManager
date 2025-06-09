dee87782e0c5c58208f706f8abe2a11662d0f51e
praneetha <gpraneetha924@gmail.com>
2025-06-09 22:57:39 +0530

    Initial commit

---
+++ src/main/java/com/automobile/company/manager/automobilecompanymanager/AutomobileCompanyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.automobile.company.manager.automobilecompanymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobileCompanyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomobileCompanyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automobile/company/manager/automobilecompanymanager/AutomobileCompanyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automobile.company.manager.automobilecompanymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobileCompanyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
